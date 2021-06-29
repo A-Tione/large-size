c13d637e30081a45c03b292f93bb6418918ae8d2
a-tione <heyanggo@gmail.com>
2021-06-29 20:24:07 +0800

    make grids

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,17 +3,15 @@ import './home.scss';
 
 export const Home = () => {
   return (
-    <div>
-      <div className="home">
-        <header/>
-        <main>
-          <section className="section1"/>
-          <section className="section2"/>
-          <section className="section3"/>
-          <section className="section4"/>
-          <section className="section5"/>
-        </main>
-      </div>
+    <div className="home">
+      <header/>
+      <main>
+        <section className="section1"/>
+        <section className="section2"/>
+        <section className="section3"/>
+        <section className="section4"/>
+        <section className="section5"/>
+      </main>
     </div>
   );
 };
