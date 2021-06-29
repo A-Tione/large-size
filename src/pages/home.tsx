58ad3a8df8f37407fc510a1783b36661c6d158b6
a-tione <heyanggo@gmail.com>
2021-06-29 20:28:38 +0800

    add background + border

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
       <header/>
       <main>
         <section className="section1"/>
-        <section className="section2"/>
+        <section className="section2">Hello</section>
         <section className="section3"/>
         <section className="section4"/>
         <section className="section5"/>
