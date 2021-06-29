c66567e9d8d68bb6f54cffc300a010cf7b4d4c60
a-tione <heyanggo@gmail.com>
2021-06-29 20:21:15 +0800

    make grids

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,13 @@ export const Home = () => {
     <div>
       <div className="home">
         <header/>
+        <main>
+          <section className="section1"/>
+          <section className="section2"/>
+          <section className="section3"/>
+          <section className="section4"/>
+          <section className="section5"/>
+        </main>
       </div>
     </div>
   );
