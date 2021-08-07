fbd63ddb2d64cf91cbd222c715776d6998401fc0
HeYang <heyanggo@gmail.com>
2021-08-07 09:48:39 +0800

    feat show

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -66,7 +66,7 @@ export const Home = () => {
           </section>
         </main>
         <footer>
-          &copy; 饥人谷 2020-{year}
+          &copy; 公安局 2020-{year}
         </footer>
       </div>
   );
